dc9f517de837ba86b8236ab6e58803bf534b60b1
redenv <redenv@careindeed.com>
2023-04-16 18:54:25 +0800

    "

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  router = inject(Router);
+
+  ngOnInit() {
+    this.router.navigate(['login']);
+  }
 }
